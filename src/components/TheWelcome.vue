9fe3971aedffc3785d8a90ea3edc12b017d54da2
rubyllamadora <ruby.lamadora@gmail.com>
2025-05-12 21:47:11 +1000

    feat: configure shadcn components
    https://www.shadcn-vue.com/docs/installation/vite.html

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
   <div>
     <h1>This is the home page</h1>
+    <div class="pt-8 space-x-4">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+    </div>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -14,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
